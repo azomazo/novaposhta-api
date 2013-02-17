9473c3caa8609b68b495882120d3cc576a5f5814
Azamat Khduaygulov <azamat@khudaygulov.ru>
2013-02-18 00:40:45 +0400

    Added gloabal api_key

--- lib/novaposhta.rb
+++ lib/novaposhta.rb
@@ -1,12 +1,21 @@
 require "novaposhta/api"
 
+require "novaposhta/city"
 require "novaposhta/warehouse"
 
 require "novaposhta/parsers/city_parser"
 require "novaposhta/parsers/warehouse_parser"
 
 module Novaposhta
-  def self.new(api_key)
-    Novaposhta::Api.new(api_key)
+  def self.new(key = nil)
+    Novaposhta::Api.new(key.nil? ? self.api_key : key)
+  end
+
+  def self.api_key
+    @@api_key
+  end
+
+  def self.api_key=(key)
+    @@api_key = key
   end
 end
